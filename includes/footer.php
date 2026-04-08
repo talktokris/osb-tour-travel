57898914546320f5a678cb84bc0bda9f951286ca
Krishhna <talktokris@gmail.com>
2026-04-08 08:27:00 +0545

    footer modified as per new compnay name

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer class="footer footer-center bg-base-300 text-base-content/80 p-6 text-sm mt-auto border-t border-base-content/10 shrink-0">
     <aside>
-        <p>&copy; <?= date('Y') ?> Within Earth Holidays Sdn Bhd. All rights reserved.</p>
+        <p>&copy; <?= date('Y') ?> OSB Global Services Sdn Bhd. All rights reserved.</p>
     </aside>
 </footer>
 </body>
